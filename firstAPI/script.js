b90d4e5bde9b5d59f762511c158f64ac5d58b498
Abdullah Naim <aanaim96@gmail.com>
2023-01-25 12:14:24 -0500

    Update script.js
    Switched from using axios to fetch. This allowed me to append the api request to the html page. The object/json was easier to work with this swapi api.

--- firstAPI/script.js
+++ firstAPI/script.js
@@ -1,3 +1,33 @@
+const button = document.querySelector('button');
+const listing = document.querySelector('ul')
+
+button.addEventListener('click', () => {
+    const person = document.createElement('p')
+    let rand = Math.floor(Math.random() * 80) + 1;
+    setTimeout(() => {
+        fetch(`https://swapi.dev/api/people/${rand}/`)
+            .then((res) => {
+                console.log('Resolved', res);
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data.name, typeof (data.name));
+                let newperson = data.name
+                person.append(newperson);
+                listing.append(person)
+            })
+            .catch((e) => {
+                console.log("error", e);
+            })
+    }, 500);
+
+
+})
+
+
+
+
+
 // fetch("https://swapi.dev/api/planets/2/")
 //     .then((res) => {
 //         console.log('Resolved', res);
@@ -25,22 +55,22 @@
 //     });
 
 
-const getStarWarsPerson = async (id) => {
-    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-    console.log(res.data.name)
-    return res;
-};
+// const getStarWarsPerson = async (id) => {
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//     console.log(res.data.name)
+//     return res;
+// };
 
-const button = document.querySelector('button');
-const listing = document.querySelector('ul')
+// const button = document.querySelector('button');
+// const listing = document.querySelector('ul')
 
-button.addEventListener('click', () => {
-    const person = document.createElement('p')
-    let rand = Math.floor(Math.random() * 80) + 1;
-    let newperson = getStarWarsPerson(rand)
-    person.append(newperson);
-    listing.append(person)
-    console.log(rand, newperson)
-})
+// button.addEventListener('click', () => {
+//     const person = document.createElement('p')
+//     let rand = Math.floor(Math.random() * 80) + 1;
+//     let newperson = getStarWarsPerson(rand)
+//     person.append(newperson);
+//     listing.append(person)
+//     console.log(rand, newperson)
+// })
 
-getStarWarsPerson(11);
+// getStarWarsPerson(11);
